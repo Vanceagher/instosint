4c12732793b44d1ee266f58093186e5640602d21
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2022-10-25 09:35:03 -0400

    first

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const puppeteer = require('puppeteer');
-const fetch = require('node-fetch');
 const fs = require('fs').promises;
+request = require('request');
+
 
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
 
-
-  // Wait for suggest overlay to appear and click "show all results".
+// load instagram cookie
   const cookieJson = await fs.readFile('./cookies.json');
       const cookies = JSON.parse(cookieJson);
       await page.setCookie(...cookies);
-      //await page.reload();
       await page.goto('https://instagram.com');
-      /*await page.waitForSelector("._a9_1")
-      await page.click("._a9_1");*/
-// //*[@id="mount_0_0_6M"]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/header/section/ul/li[2]/a/div
-      //document.querySelectorAll("._aacp")[1];
+      
+      var igUsername = 'kanyewest';
 
-      var id = ;
-      var count = 100;
+      var id = await page.evaluate(async (igUsername) => {
+        const response = await fetch('https://www.instagram.com/web/search/topsearch/?context=user&count=0&query=' + igUsername);
+        const text = await response.text();
+        return text;
+      }, igUsername);
+
+      console.log(JSON.parse(id).users[0].user.pk);
+      
+      id = JSON.parse(id).users[0].user.pk;
+      var count = 25;
 
       const followers = await page.evaluate((id, count) => {
         const random_wait_time = (waitTime = 300) => new Promise((resolve, reject) => {
@@ -44,7 +49,7 @@ const fs = require('fs').promises;
                 console.log(res);
                 const nodeIds = [];
                 for (const node of res.data.user.edge_followed_by.edges) {
-                  nodeIds.push(node.node.id);
+                  nodeIds.push(node.node.username);
                 }
                 actuallyFetched = nodeIds.length;
                 return {
@@ -71,5 +76,19 @@ const fs = require('fs').promises;
       }, id, count);
 
       console.log(followers);
+
+      await page.goto('https://instagram.com/' + igUsername);
+      await page.waitForSelector('._aabd');
+
+      const photos = await page.evaluate(() => {
+        var photos = [];
+        var srcs = document.querySelectorAll('._aabd img');
+        for (let i = 0; i < srcs.length; i++) {
+          photos.push(srcs[i].src);
+        }
+        return photos;
+     });
+
+     console.log(photos);
 
 })();
